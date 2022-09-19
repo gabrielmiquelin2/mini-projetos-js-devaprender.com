dc8e2c9ebf6489551d6de897bb2a9320ffebc234
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-09-19 16:05:09 -0300

    commit

---
+++ maximo-entre-dois-valores/scrpt.js
@@ -0,0 +1,13 @@
+//Escreva uma função que usa 2 número e retonrna o maior entre eles
+
+let valorMaior = (31,36);
+console.log(valorMaior);
+
+function max (numero1,numero2){
+  //  if(numero1 > numero2)
+  //     return numero1;
+  //  return numero2;
+
+   //utlizando operador ternário para resolver o problema
+  return numero1>numero2 ? numero1: numero2 ;
+}
