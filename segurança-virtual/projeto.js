e960feb15c3b9f71db89eaac47efa0467069ab5a
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-09-30 14:21:36 -0300

    commit

--- segurança-virtual/projeto.js
+++ segurança-virtual/projeto.js
@@ -1,7 +1,7 @@
 function VerificarEntrada(){
     NomeConvidado = document.getElementById('nome').value;
-    MeusConvidados = ['Lucas','Mathues','José','Laura']
-    if(MeusConvidados.includes(NomeConvidado)){
+    convidados = ['Lucas','Mathues','José','Laura']
+    if(convidados.includes(NomeConvidado)){
        document.getElementById('PermissaoDeEntrada').innerText ="Voê PODE Entrar!"
     }else{
       document.getElementById('PermissaoDeEntrada').innerText = 'Você NÃO pode entrar!'
